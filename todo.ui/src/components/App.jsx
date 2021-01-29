b8a4b01f0e134aad3cbea57fb29f80523232a255
pauljmelia <paul@melia.co.za>
2021-01-29 12:56:57 +0200

    setup: restructuring project

---
+++ todo.ui/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import logo from './logo.svg';
+
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Edit src/App.js and save to reload.</p>
+      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </header>
+  </div>
+);
